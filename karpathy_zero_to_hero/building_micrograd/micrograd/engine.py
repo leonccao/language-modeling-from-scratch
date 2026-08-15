e37c52ca15c7b31bd7e3c2584fd7063a687c3d3e
Minty <mintyck@gmail.com>
2026-08-15 00:51:55 -0400

    Implement Value arithmetic operations

--- karpathy_zero_to_hero/building_micrograd/micrograd/engine.py
+++ karpathy_zero_to_hero/building_micrograd/micrograd/engine.py
@@ -9,15 +9,9 @@ class Value:
     _op: str
     _backward: Callable[[], None]
 
-    """
-    TODO
-    - backward
-    - a * 2 -> 2 * a
-    """
-
     def __init__(self, data, _children=(), _op=""):
         """Initialize a graph node, its gradient, and its graph metadata."""
-        self.data = data
+        self.data = float(data)
         self.grad = 0
         self._prev = set(_children)
         self._op = _op
@@ -34,17 +28,34 @@ class Value:
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
-        out.backward = _backward
+        out._backward = _backward
 
         return out
 
     def __mul__(self, other):
         """Return the product as a new graph-connected Value."""
-        raise NotImplementedError("TODO: implement Value.__mul__")
+
+        if not isinstance(other, Value):
+            other = Value(other)
+        out = Value(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+
+        return out
 
     def __pow__(self, other):
         """Raise this Value to an int or float power."""
-        raise NotImplementedError("TODO: implement Value.__pow__")
+        assert not isinstance(other, Value), "Does not support Value power"
+        out = Value(self.data ** other, (self,), "**")
+
+        def _backward():
+            self.grad += other * self.data ** (other - 1) * out.grad
+        out._backward = _backward
+
+        return out
 
     def relu(self):
         """Apply the scalar rectified linear unit operation."""
@@ -60,31 +71,31 @@ class Value:
 
     def __neg__(self):
         """Return the arithmetic negation of this Value."""
-        raise NotImplementedError("TODO: implement Value.__neg__")
+        return self.__mul__(-1)
 
     def __radd__(self, other):
         """Support scalar addition with the scalar on the left."""
-        raise NotImplementedError("TODO: implement Value.__radd__")
+        return self.__add__(other)
 
     def __sub__(self, other):
         """Return the result of subtracting other from this Value."""
-        raise NotImplementedError("TODO: implement Value.__sub__")
+        return self.__add__(other.__neg__())
 
     def __rsub__(self, other):
         """Support subtraction from a scalar on the left."""
-        raise NotImplementedError("TODO: implement Value.__rsub__")
+        return self.__neg__().__add__(other)
 
     def __rmul__(self, other):
         """Support scalar multiplication with the scalar on the left."""
-        raise NotImplementedError("TODO: implement Value.__rmul__")
+        return self.__mul__(other)
 
     def __truediv__(self, other):
         """Return the result of dividing this Value by other."""
-        raise NotImplementedError("TODO: implement Value.__truediv__")
+        return self.__mul__(other.__pow__(-1))
 
     def __rtruediv__(self, other):
         """Support division of a scalar by this Value."""
-        raise NotImplementedError("TODO: implement Value.__rtruediv__")
+        return self.__pow__(-1).__mul__(other)
 
     def __repr__(self) -> str:
         """Return the upstream-compatible data and gradient representation."""
